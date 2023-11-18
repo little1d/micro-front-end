14e12ef48a3bfa1200ed4b952493a2c632bc3061
yang_zhuo <2431542525@qq.com>
2023-11-18 21:13:22 +0800

    bugfixed(vue)：正常添加vue项目

--- qiankun-vue-micro-app3/vite.config.js
+++ qiankun-vue-micro-app3/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    qiankun('qiankun-vue-micro-app3', {
+      useDevMode: true
+    })
+  ],
+  server: {
+    port: 5173,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
